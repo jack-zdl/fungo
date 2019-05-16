afebffa1fbee8a8f54f3b66a4a3e25509425e6f2
mxf <m-java@163.com>
2019-05-16 15:23:06 +0800

    社区依赖系统、用户接口

---
+++ fungo-game-community/src/main/java/com/fungo/community/ts/mq/service/MQDataReceiveServiceImpl.java
@@ -0,0 +1,43 @@
+package com.fungo.community.ts.mq.service;
+
+import com.game.common.ts.mq.service.MQDataReceiveService;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+
+@Service("mQDataReceiveService")
+public class MQDataReceiveServiceImpl implements MQDataReceiveService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MQDataReceiveServiceImpl.class);
+
+
+
+    @Override
+    public void onMessageWithMQDirect(String msgData) throws Exception {
+        if (StringUtils.isBlank(msgData)) {
+            return;
+        }
+        try {
+            LOGGER.info("MQDataReceiveServiceImpl-onMessageWithMQDirect-msg:{}", msgData);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onMessageWithMQTopic(String msgData) {
+        if (StringUtils.isBlank(msgData)) {
+            return;
+        }
+        try {
+            //调用业务方处理消息
+
+            LOGGER.info("MQDataReceiveServiceImpl-onMessageWithMQTopic-msg:{}", msgData);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
