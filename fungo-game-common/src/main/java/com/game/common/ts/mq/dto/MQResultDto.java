1fdc6ed694d7dc0957babf74330bb0ce69cd5a63
mxf <m-java@163.com>
2019-05-17 13:57:19 +0800

    扣减用户经验值和等级枚举和数据结构定义

--- fungo-game-common/src/main/java/com/game/common/ts/mq/dto/MQResultDto.java
+++ fungo-game-common/src/main/java/com/game/common/ts/mq/dto/MQResultDto.java
@@ -27,4 +27,43 @@ public class MQResultDto {
     public void setBody(Object body) {
         this.body = body;
     }
+
+
+    public enum CommunityEnum {
+
+        /**
+         * 扣减用户经验值和等级,MQ消息数据：
+         * {
+         *     "consumerQueue": "msgFungoTopicQueue_community_POST",
+         *     "filter": "",
+         *     "limit": 10,
+         *     "messageBody": "{"body":{"score":3,"mb_id":"00167ecb60374a439431563401285e58"},"type":1}",
+         *     "messageDataType": 3,
+         *     "page": 1,
+         *     "pageNum": 1,
+         *     "pageSize": 10,
+         *     "sort": 0
+         * }
+         */
+        CMT_POST_MQ_TYPE_DELETE_POST_SUBTRACT_EXP_LEVEL(1, "扣减用户经验值和等级");
+
+
+        private int code;
+        private String name;
+
+        private CommunityEnum(int code, String name) {
+            this.code = code;
+            this.name = name;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    //---------
 }
