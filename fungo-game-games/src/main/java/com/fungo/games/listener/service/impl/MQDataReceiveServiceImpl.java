4922b5e5f405baba2485d0085f79a334271ade6c
lycyc <xb7427748naita@163.com>
2019-05-17 14:11:51 +0800

    临时

--- fungo-game-games/src/main/java/com/fungo/games/listener/service/impl/MQDataReceiveServiceImpl.java
+++ fungo-game-games/src/main/java/com/fungo/games/listener/service/impl/MQDataReceiveServiceImpl.java
@@ -1,10 +1,15 @@
 package com.fungo.games.listener.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.game.common.dto.GameDto;
+import com.game.common.ts.mq.dto.MQResultDto;
+import com.game.common.ts.mq.dto.TransactionMessageDto;
 import com.game.common.ts.mq.service.MQDataReceiveService;
+import com.game.common.ts.mq.service.MQDataSendService;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 
@@ -36,7 +41,13 @@ public class MQDataReceiveServiceImpl implements MQDataReceiveService {
             System.out.println("我拿到了");
             //调用业务方处理消息
             LOGGER.info("MQDataReceiveServiceImpl-onMessageWithMQTopic-msg:{}", msgData);
-            JSON.parseObject(msgData);
+            TransactionMessageDto transactionMessageDto = JSON.parseObject(msgData, TransactionMessageDto.class);
+            String messageBody = transactionMessageDto.getMessageBody();
+            MQResultDto mqResultDto = JSON.parseObject(messageBody, MQResultDto.class);
+//            if (mqResultDto.getType()==)
+            Object body = mqResultDto.getBody();
+            GameDto gameDto1 = JSON.parseObject(body.toString(), GameDto.class);
+            System.out.println(gameDto1.toString());
             System.out.println("我拿完了");
         } catch (Exception ex) {
             ex.printStackTrace();
