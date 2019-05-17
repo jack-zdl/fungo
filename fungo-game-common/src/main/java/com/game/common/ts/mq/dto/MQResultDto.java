805be2deedf080df04357a6b90d6f07bafb57977
fungo-jack <1993212>
2019-05-17 13:45:00 +0800

    mq枚举类添加

--- fungo-game-common/src/main/java/com/game/common/ts/mq/dto/MQResultDto.java
+++ fungo-game-common/src/main/java/com/game/common/ts/mq/dto/MQResultDto.java
@@ -27,4 +27,30 @@ public class MQResultDto {
     public void setBody(Object body) {
         this.body = body;
     }
+
+    public enum  SystemMQDataType{
+        //默认
+        SYSTEM_DATA_TYPE_GAMEINSERT(1, "com.game.common.dto.GameDto"),
+        SYSTEM_DATA_TYPE_GAMEUPDATE(2, "com.game.common.dto.GameDto"),
+        SYSTEM_DATA_TYPE_GAMERELEASELOG(3, "com.game.common.dto.game.GameReleaseLogDto"),
+        SYSTEM_DATA_TYPE_counter(4, "   java.util.HashMap"),
+        SYSTEM_DATA_TYPE_COMMUNITYINSERT(5, "com.game.common.dto.community.CmmCommunityDto");
+
+        private int code;
+        private String name;
+
+        SystemMQDataType(int code, String name) {
+            this.code = code;
+            this.name = name;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+    }
 }
