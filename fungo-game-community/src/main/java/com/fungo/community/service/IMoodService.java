74c24744f1b85ff3a415b524828b6453336acc9a
mxf <m-java@163.com>
2019-05-18 11:10:01 +0800

    心情微服务发布心情执行任务MQ

--- fungo-game-community/src/main/java/com/fungo/community/service/IMoodService.java
+++ fungo-game-community/src/main/java/com/fungo/community/service/IMoodService.java
@@ -7,10 +7,15 @@ import com.game.common.dto.community.MoodInput;
 import com.sun.corba.se.spi.ior.ObjectId;
 
 public interface IMoodService {
+
 	//发布心情
 	public ResultDto<ObjectId> addMood(String memberId, MoodInput input) throws Exception;
+
+
 	//删除心情
 	public ResultDto<String> delMood(String memberId, String moodId);
+
+
 	//心情详情
 	public ResultDto<MoodBean> getMood(String memberId, String moodId) throws Exception;
 }
