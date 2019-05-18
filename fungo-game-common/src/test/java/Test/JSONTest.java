3286e97f79839b1f332b41b9249240bdf89d7ea4
mxf <m-java@163.com>
2019-05-18 13:33:14 +0800

    junit

--- fungo-game-common/src/test/java/Test/JSONTest.java
+++ fungo-game-common/src/test/java/Test/JSONTest.java
@@ -1,13 +1,13 @@
 package Test;
 
 import com.alibaba.fastjson.JSON;
-import com.game.common.dto.action.BasActionDto;
+import com.game.common.consts.MemberIncentTaskConsts;
+import com.game.common.dto.system.TaskDto;
+import com.game.common.enums.FunGoIncentTaskV246Enum;
 import com.game.common.ts.mq.dto.MQResultDto;
 import com.game.common.ts.mq.dto.TransactionMessageDto;
 import com.game.common.ts.mq.enums.RabbitMQEnum;
 import org.junit.Test;
-
-import java.util.Date;
 
 public class JSONTest {
 
@@ -15,14 +15,12 @@ public class JSONTest {
 
     @Test
     public void testJson(){
-        BasActionDto basActionDtoAdd = new BasActionDto();
-        basActionDtoAdd.setCreatedAt(new Date());
-        basActionDtoAdd.setUpdatedAt(new Date());
-        basActionDtoAdd.setMemberId("01d1c44a2eda4fd3a1fe82e81abab9cf");
-        basActionDtoAdd.setType(11);
-        basActionDtoAdd.setTargetType(1);
-        basActionDtoAdd.setTargetId("01d1c44a2eda4fd3a1fe82e81abab9cf");
-        basActionDtoAdd.setState(0);
+        TaskDto taskDtoExp = new TaskDto();
+        taskDtoExp.setRequestId("06f2f38999204b48a6b24429b8014455");
+        taskDtoExp.setMbId("01d1c44a2eda4fd3a1fe82e81abab9cf");
+        taskDtoExp.setTaskGroupFlag(FunGoIncentTaskV246Enum.TASK_GROUP_EVERYDAY.code());
+        taskDtoExp.setTaskType(MemberIncentTaskConsts.INECT_TASK_SCORE_EXP_CODE_IDT);
+        taskDtoExp.setTypeCodeIdt(FunGoIncentTaskV246Enum.TASK_GROUP_EVERYDAY_FISRT_SEND_ARTICLE_EXP.code());
 
         TransactionMessageDto transactionMessageDto = new TransactionMessageDto();
 
@@ -35,14 +33,14 @@ public class JSONTest {
         //路由key
         StringBuffer routinKey = new StringBuffer(RabbitMQEnum.QueueRouteKey.QUEUE_ROUTE_KEY_TOPIC_SYSTEM_USER.getName());
         routinKey.deleteCharAt(routinKey.length() - 1 );
-        routinKey.append("ACTION_ADD");
+        routinKey.append("cmtPostMQDoTask");
 
         transactionMessageDto.setRoutingKey(routinKey.toString());
 
         MQResultDto mqResultDto = new MQResultDto();
         mqResultDto.setType(MQResultDto.CommunityEnum.CMT_ACTION_MQ_TYPE_ACTION_ADD.getCode());
 
-        mqResultDto.setBody(basActionDtoAdd);
+        mqResultDto.setBody(taskDtoExp);
 
         transactionMessageDto.setMessageBody(JSON.toJSONString(mqResultDto));
         System.out.println("----------------: "+ JSON.toJSONString(transactionMessageDto));
