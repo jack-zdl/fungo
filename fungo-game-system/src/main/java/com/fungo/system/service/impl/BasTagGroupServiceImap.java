554fdfec18aaea13ea1e5730f81932935a05d902
yangshaoxiang <1330160658@qq.com>
2019-05-18 15:22:16 +0800

    新增部分社区和游戏依赖接口

---
+++ fungo-game-system/src/main/java/com/fungo/system/service/impl/BasTagGroupServiceImap.java
@@ -0,0 +1,20 @@
+package com.fungo.system.service.impl;
+
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.fungo.system.dao.BasTagGroupDao;
+import com.fungo.system.entity.BasTagGroup;
+import com.fungo.system.service.BasTagGroupService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 标签组 服务实现类
+ * </p>
+ *
+ * @author lzh
+ * @since 2018-05-09
+ */
+@Service
+public class BasTagGroupServiceImap extends ServiceImpl<BasTagGroupDao, BasTagGroup> implements BasTagGroupService {
+	
+}
