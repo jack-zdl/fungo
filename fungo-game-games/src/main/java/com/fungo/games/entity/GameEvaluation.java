43e5e5a961f05526b1ac389ccb0538cc5ece0757
lycyc <xb7427748naita@163.com>
2019-05-21 10:15:11 +0800

    Merge branch 'Branch_game'
    # Conflicts:
    #	fungo-game-games/src/main/resources/config/application-uat.yml
    #	fungo-game-gateway/src/main/resources/application.yml

--- fungo-game-games/src/main/java/com/fungo/games/entity/GameEvaluation.java
+++ fungo-game-games/src/main/java/com/fungo/games/entity/GameEvaluation.java
@@ -2,7 +2,9 @@ package com.fungo.games.entity;
 
 import com.baomidou.mybatisplus.activerecord.Model;
 import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.annotations.TableName;
+import com.baomidou.mybatisplus.enums.IdType;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -20,6 +22,7 @@ public class GameEvaluation extends Model<GameEvaluation> {
 
     private static final long serialVersionUID = 1L;
 
+	@TableId(value = "id",type = IdType.UUID)
 	private String id;
     /**
      * 游戏ID
