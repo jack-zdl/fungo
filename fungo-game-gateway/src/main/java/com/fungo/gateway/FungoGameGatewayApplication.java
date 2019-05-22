65164324046f795017ed80fdf3da09019818eb8e
mxf <m-java@163.com>
2019-05-22 13:18:46 +0800

    文件上传大小配置

--- fungo-game-gateway/src/main/java/com/fungo/gateway/FungoGameGatewayApplication.java
+++ fungo-game-gateway/src/main/java/com/fungo/gateway/FungoGameGatewayApplication.java
@@ -3,9 +3,12 @@ package com.fungo.gateway;
 import com.fungo.gateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 
 @EnableZuulProxy    //开启Zuul的API网关服务
@@ -21,4 +24,19 @@ public class FungoGameGatewayApplication {
 	public AccessFilter loginFilter() {
 		return new AccessFilter();
 	}
+
+	/**
+	 * 文件上传配置
+	 * @return
+	 */
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		//文件最大  KB,MB-10MB
+		factory.setMaxFileSize("10240KB");
+		/// 设置总上传数据总大小 100MB
+		factory.setMaxRequestSize("102400KB");
+		return factory.createMultipartConfig();
+	}
+
 }
