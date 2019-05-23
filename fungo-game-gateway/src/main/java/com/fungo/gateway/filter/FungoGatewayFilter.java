1e3228efb47fa4b6b19a46697936ed9190f6559d
mxf <m-java@163.com>
2019-05-23 13:02:18 +0800

    gateway

---
+++ fungo-game-gateway/src/main/java/com/fungo/gateway/filter/FungoGatewayFilter.java
@@ -0,0 +1,38 @@
+package com.fungo.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 过滤器
+ */
+public class FungoGatewayFilter implements GatewayFilter, Ordered {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FungoGatewayFilter.class);
+    private static final String REQUEST_TIME_BEGIN = "requestTimeBegin";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        exchange.getAttributes().put(REQUEST_TIME_BEGIN, System.currentTimeMillis());
+        return chain.filter(exchange).then(
+                Mono.fromRunnable(() -> {
+                    Long startTime = exchange.getAttribute(REQUEST_TIME_BEGIN);
+                    if (startTime != null) {
+                        LOGGER.info("------------------过滤器:{}", exchange.getRequest().getURI().getRawPath() + " : " + (System.currentTimeMillis() - startTime) + "ms");
+                    }
+                })
+        );
+
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
