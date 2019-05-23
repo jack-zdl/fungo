7f74c29c523b41645693082d2c5b7ad34e232cb1
mxf <m-java@163.com>
2019-05-23 17:01:03 +0800

    解决冲突

--- fungo-game-gateway/src/main/java/com/fungo/gateway/FungoGameGatewayApplication.java
+++ fungo-game-gateway/src/main/java/com/fungo/gateway/FungoGameGatewayApplication.java
@@ -7,16 +7,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 
-<<<<<<< HEAD
-
-=======
-@EnableEurekaClient
->>>>>>> remotes/origin/Branch_communityV2
 @SpringBootApplication
 @EnableEurekaClient
 public class FungoGameGatewayApplication {
 
-<<<<<<< HEAD
     public static void main(String[] args) {
         SpringApplication.run(FungoGameGatewayApplication.class, args);
     }
@@ -25,17 +19,6 @@ public class FungoGameGatewayApplication {
     public FungoGatewayFilter gatewayFilter() {
         return new FungoGatewayFilter();
     }
-=======
-	public static void main(String[] args) {
-		SpringApplication.run(FungoGameGatewayApplication.class, args);
-	}
-
-
-	@Bean
-	public FungoGatewayFilter gatewayFilter() {
-		return new FungoGatewayFilter();
-	}
->>>>>>> remotes/origin/Branch_communityV2
 
 
 }
