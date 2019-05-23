ea7b7d2ee77639afeac3ca64376d743328fa8fe9
mxf <m-java@163.com>
2019-05-23 15:59:38 +0800

    配置修改

---
+++ fungo-game-community/src/main/java/com/fungo/community/service/impl/RequestLoggerServiceImpl.java
@@ -0,0 +1,46 @@
+package com.fungo.community.service.impl;
+
+import com.fungo.games.entity.BasLog;
+import com.game.common.dto.WebLog;
+import com.game.common.log.IRequestLogger;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RequestLoggerServiceImpl implements IRequestLogger {
+
+	@Override
+	public void log(WebLog wlog) {
+		BasLog log=new BasLog();
+		log.setBizCode(wlog.getBizCode());
+		log.setBizId(wlog.getBizId());
+		log.setBrand(wlog.getBrand());
+		log.setChannel(wlog.getChannel());
+		log.setCreatedAt(wlog.getCreatedAt());
+		log.setEndTime(wlog.getEndTime());
+		log.setInputData(wlog.getInputData());
+		log.setIp(wlog.getIp());
+		log.setMemberId(wlog.getMemberId());
+		log.setMethod(wlog.getMethod());
+		log.setOutData(wlog.getOutData());
+
+		String path = wlog.getPath();
+		if (null != path && path.length() > 0) {
+			path = path.substring(0, path.lastIndexOf("/"));
+		}
+		log.setPath(path);
+
+
+		log.setReCode(wlog.getReCode());
+		log.setRunTime(wlog.getRunTime());
+		log.setUpdatedAt(wlog.getUpdatedAt());
+		log.setToken(wlog.getToken());
+		
+		log.setAppversion(wlog.getAppversion());
+		log.setHeight(wlog.getHeight());
+		log.setWidth(wlog.getWidth());
+		log.setVersion(wlog.getVersion());
+		log.setUdid(wlog.getUdid());
+		log.insert();
+	}
+
+}
