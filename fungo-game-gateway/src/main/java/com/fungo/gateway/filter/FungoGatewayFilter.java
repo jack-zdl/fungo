a956abddf2ed6882de6057d3fd99de9e0cc097c5
mxf <m-java@163.com>
2019-05-23 14:41:43 +0800

    配置修改

---
+++ fungo-game-gateway/src/main/java/com/fungo/gateway/filter/FungoGatewayFilter.java
@@ -0,0 +1,33 @@
+package com.fungo.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 过滤器
+ */
+public class FungoGatewayFilter implements GatewayFilter, Ordered {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FungoGatewayFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        return chain.filter(exchange).then(
+                Mono.fromRunnable(() -> {
+                    LOGGER.info("------------------过滤器:{}", exchange.getRequest().getURI().getRawPath());
+                })
+        );
+
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
