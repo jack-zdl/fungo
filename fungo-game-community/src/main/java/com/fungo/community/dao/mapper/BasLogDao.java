a605e6d511a2e6022b8d1c6c83054340c5835fe2
mxf <m-java@163.com>
2019-05-23 17:03:38 +0800

    日志mapper

---
+++ fungo-game-community/src/main/java/com/fungo/community/dao/mapper/BasLogDao.java
@@ -0,0 +1,16 @@
+package com.fungo.community.dao.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.fungo.community.entity.BasLog;
+
+/**
+ * <p>
+  * 接口前台访问日志 Mapper 接口
+ * </p>
+ *
+ * @author lzh
+ * @since 2018-07-09
+ */
+public interface BasLogDao extends BaseMapper<BasLog> {
+
+}
