d913db155a47e9834a7427588b5cfa95bfd07e03
fungo-jack <1993212>
2019-05-27 10:45:28 +0800

    修改gateway

--- fungo-game-gateway/src/main/java/com/fungo/gateway/FungoGameGatewayApplication.java
+++ fungo-game-gateway/src/main/java/com/fungo/gateway/FungoGameGatewayApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
@@ -15,7 +14,6 @@ import javax.servlet.MultipartConfigElement;
 @EnableZuulProxy    //开启Zuul的API网关服务
 @EnableEurekaClient
 @SpringBootApplication
-@EnableHystrixDashboard
 public class FungoGameGatewayApplication {
 
 	public static void main(String[] args) {
--- fungo-game-system/src/main/java/com/fungo/system/controller/portal/PortalSystemController.java
+++ fungo-game-system/src/main/java/com/fungo/system/controller/portal/PortalSystemController.java
@@ -1,22 +1,9 @@
 package com.fungo.system.controller.portal;
 
-
-import com.baomidou.mybatisplus.mapper.EntityWrapper;
-import com.fungo.system.entity.Banner;
 import com.fungo.system.service.BannerService;
-import com.game.common.dto.ResultDto;
-import com.game.common.dto.advert.AdvertOutBean;
 import com.game.common.repo.cache.facade.FungoCacheAdvert;
-import com.game.common.util.CommonUtils;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *  PC端门户-首页
@@ -34,51 +21,5 @@ public class PortalSystemController {
     private FungoCacheAdvert fungoCacheAdvert;
 
 
-    @ApiOperation(value = "首页游戏banner接口(v2.3)", notes = "")
-    @ApiImplicitParams({})
-    @RequestMapping(value = "/api/portal/index/adt/bnr", method = RequestMethod.GET)
-    public ResultDto<List<AdvertOutBean>> getAdvertWithPc() {
 
-        ResultDto<List<AdvertOutBean>> re = new ResultDto<List<AdvertOutBean>>();
-        //from redis
-        List<AdvertOutBean> list = (List<AdvertOutBean>) fungoCacheAdvert.getIndexCache("/api/portal/index/adt/bnr", "");
-        if (null != list && !list.isEmpty()) {
-            re.setData(list);
-            //return re;
-        }
-
-        //获取广告位
-        List<Banner> blist = bannerService.selectList(new EntityWrapper<Banner>().eq("position_code", "0001").eq("state", 0).orderBy("sort", false));
-        if (null != blist && !blist.isEmpty()) {
-            list = new ArrayList<AdvertOutBean>();
-            for (Banner banner : blist) {
-                AdvertOutBean bean = new AdvertOutBean();
-                bean.setBizId(banner.getTargetId());
-                bean.setBizType(1);
-                bean.setContent(CommonUtils.filterWord(banner.getIntro()));
-                bean.setImageUrl(banner.getCoverImage());
-                bean.setName(banner.getTag());
-                bean.setTitle(CommonUtils.filterWord(banner.getTitle()));
-
-                list.add(bean);
-            }
-            re.setData(list);
-            //redis cache
-            fungoCacheAdvert.excIndexCache(true, "/api/portal/index/adt/bnr", "", list);
-        }
-        return re;
-    }
-
-
-
-//
-//    @ApiOperation(value = "广告位置活动位 | 专属活动", notes = "")
-//    @ApiImplicitParams({})
-//    @RequestMapping(value = "/api/portal/index/adt/activity", method = RequestMethod.GET)
-//    public ResultDto<List<AdvertOutBean>> getAdvertWithPc() {
-//
-//        return null;
-//    }
-
-//---------
 }
