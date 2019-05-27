735c21f1efd0715c707fcd33fb4704814215287e
fungo-jack <1993212>
2019-05-27 10:54:23 +0800

    修改gateway的依赖，改为gateway

--- fungo-game-gateway/src/main/java/com/fungo/gateway/FungoGameGatewayApplication.java
+++ fungo-game-gateway/src/main/java/com/fungo/gateway/FungoGameGatewayApplication.java
@@ -1,49 +1,27 @@
 package com.fungo.gateway;
 
-import com.fungo.gateway.filter.AccessFilter;
+import com.fungo.gateway.filter.FungoGatewayFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
-import javax.servlet.MultipartConfigElement;
 
 
-@EnableZuulProxy    //开启Zuul的API网关服务
-@EnableEurekaClient
 @SpringBootApplication
+@EnableEurekaClient
 public class FungoGameGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FungoGameGatewayApplication.class, args);
 	}
 
-
 	@Bean
-	public AccessFilter loginFilter() {
-		return new AccessFilter();
-	}
-
-	/**
-	 * 文件上传配置
-	 * @return
-	 */
-	@Bean
-	public MultipartConfigElement multipartConfigElement() {
-		MultipartConfigFactory factory = new MultipartConfigFactory();
-		//文件最大  KB,MB-10MB
-		factory.setMaxFileSize("10240KB");
-		/// 设置总上传数据总大小 100MB
-		factory.setMaxRequestSize("102400KB");
-		return factory.createMultipartConfig();
+	public FungoGatewayFilter gatewayFilter() {
+		return new FungoGatewayFilter();
 	}
 
 
-//	@Bean
-//	public AccessFilter loginFilter() {
-//		return new AccessFilter();
-//	}
+
 
 }
