3372eeda7dbe2818fd255227c8a0099849f54e0a
fungo-jack <1993212>
2019-11-07 10:36:44 +0800

    删除反馈添加zk分布式锁/删除重复代码/删除从任务日志库查询是否有记录

--- fungo-game-system/src/main/java/com/fungo/system/service/impl/MemberIncentDoTaskFacadeServiceImpl.java
+++ fungo-game-system/src/main/java/com/fungo/system/service/impl/MemberIncentDoTaskFacadeServiceImpl.java
@@ -54,12 +54,6 @@ public class MemberIncentDoTaskFacadeServiceImpl implements IMemberIncentDoTaskF
             resMap.put("success", false);
             return resMap;
         }
-        if(StringUtil.isNull(mb_id)){
-            logger.error("用户id为null，任务执行失败");
-            resMap.put("-1", "服务器非常繁忙，请耐心等一下");
-            resMap.put("success", false);
-            return resMap;
-        }
 
         try {
             logger.info("执行任务-member:{}---task_group_flag:{}---task_type:{}---type_code_idt:{}", mb_id, task_group_flag, task_type, type_code_idt);
