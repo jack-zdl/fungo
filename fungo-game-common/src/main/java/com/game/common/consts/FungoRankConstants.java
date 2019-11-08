cd781ebe4cd8f2b634b3ca2523615ac9220da9c3
Carlos_yanglei <18226988031@163.com>
2019-11-08 11:23:35 +0800

    2.6

---
+++ fungo-game-common/src/main/java/com/game/common/consts/FungoRankConstants.java
@@ -0,0 +1,31 @@
+package com.game.common.consts;
+
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Carlos
+ * @since 2018-12-04
+ */
+public class FungoRankConstants {
+
+    /**
+     * 资源类型
+     */
+    public static final int SELECTED_TWENTY_FIVE = 25;//文章上精选2次
+    public static final int SELECTED_TWENTY_SIX = 26;//文章上精选10次
+    public static final int SELECTED_TWENTY_SEVEN = 27;//文章上精选50次
+    public static final int AMWAYWALL_TWENTY_EIGHT = 28;//评测上安利墙2次
+    public static final int AMWAYWALL_TWENTY_NINE = 29;//评测上安利墙10次
+    public static final int AMWAYWALL_THIRTY = 30;//评测上安利墙50次
+    public static final int LIKE_THIRTY_ONE = 31;//累计收到点赞50次
+    public static final int LIKE_THIRTY_TWO = 32;//累计收到点赞100次
+    public static final int LIKE_THIRTY_THREE = 33;//累计收到点赞300次
+    public static final int SIGN_THIRTY_FOUR = 34;//累计签到7天
+    public static final int SIGN_THIRTY_FIVE = 35;//累计签到30天
+    public static final int SIGN_THIRTY_SIX = 36;//累计签到100天
+
+
+}
