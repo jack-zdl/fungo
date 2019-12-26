02b56229944fbdce088cc5197ea60f66833921cf
fungo-jack <1993212>
2019-12-26 11:06:40 +0800

    修改删除缓存

--- fungo-game-community/src/main/java/com/fungo/community/service/impl/PostServiceImpl.java
+++ fungo-game-community/src/main/java/com/fungo/community/service/impl/PostServiceImpl.java
@@ -561,9 +561,9 @@ public class PostServiceImpl implements IPostService {
         //我的文章(2.4.3)
         fungoCacheArticle.excIndexCache(false, FungoCoreApiConstant.FUNGO_CORE_API_MEMBER_USER_POSTS, "", null);
         //刪除文章相關的rediskey
-        fungoCacheArticle.excIndexDecodeCache( false, FungoCoreApiConstant.PUB_POST, "", null,null);
+        fungoCacheArticle.removeIndexDecodeCache( false, FungoCoreApiConstant.PUB_POST);
         //刪除文章相關的社区信息，文章数据
-        fungoCacheArticle.excIndexDecodeCache( false, FungoCoreApiConstant.PUB_CIRCLE, "", null,null);
+        fungoCacheArticle.removeIndexDecodeCache( false, FungoCoreApiConstant.PUB_CIRCLE);
         //添加埋点信息
         BuriedPointPostModel model = new BuriedPointPostModel();
         //封装父类3个公共属性
