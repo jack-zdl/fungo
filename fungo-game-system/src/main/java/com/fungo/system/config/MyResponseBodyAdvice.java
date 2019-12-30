3a9ffb1e2a577b756a7d4a88d44a9f8fd2f9fba7
zhangdelei <zhangdelei000@gmail.com>
2019-12-30 08:00:39 +0800

    修改response返回值

--- fungo-game-system/src/main/java/com/fungo/system/config/MyResponseBodyAdvice.java
+++ fungo-game-system/src/main/java/com/fungo/system/config/MyResponseBodyAdvice.java
@@ -2,10 +2,12 @@ package com.fungo.system.config;
 
 import com.alibaba.fastjson.JSON;
 import com.fungo.system.dto.UserBean;
+import com.game.common.util.annotation.JsonView;
 import lombok.extern.slf4j.Slf4j;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -31,7 +33,7 @@ public class MyResponseBodyAdvice implements ResponseBodyAdvice {
 
     @Override
     public boolean supports(MethodParameter methodParameter, Class aClass) {
-        return true;
+        return   methodParameter.getParameterAnnotation(JsonView.class) != null ;
     }
 
     @Override
